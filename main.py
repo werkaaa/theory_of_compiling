44c493851e39095add6ace95521c1c308d2dddd4
Kamilbur <kamil.burkiewicz@gmail.com>
2020-10-17 19:46:23 +0200

    Moved main code to a main function. Added a path to the location of tests. New format of output.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
 import sys
 import scanner
 
-if __name__ == '__main__':
+# For portability
+default_test_file_path = ['tests', 'example_full.txt']
 
+def main():
   try:
-    filename = sys.argv[1] if len(sys.argv) > 1 else "example_full.txt"
+    filename = sys.argv[1] if len(sys.argv) > 1 else os.path.join(*default_test_file_path) 
     file = open(filename, "r")
   except IOError:
     print("Cannot open {0} file".format(filename))
@@ -21,4 +24,8 @@ if __name__ == '__main__':
     tok = lexer.token()
     if not tok:
       break  # No more input
-    print(tok)
+    print(f'({tok.lineno}): {tok.type}({tok.value})')
+
+if __name__ == '__main__':
+    main()
+
